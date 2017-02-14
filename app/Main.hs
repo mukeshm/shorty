df10586e47c2bfcdc638f50fa84a84b8f7411c8c
Mukesh.M <mukeshgowda.m@gmail.com>
2017-02-14 18:17:58 +0530

    Scotty says hello

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,10 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main where
 
+import Web.Scotty
+
 main :: IO ()
-main = putStrLn "Hello from shorty!!!!"
+main = scotty 8080 $ do
+  get "/" $ do
+    html "hello from scotty"
